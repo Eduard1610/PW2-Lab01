22bf538810877424d1e848c09af40ebe3ed78c85
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-04-28 11:48:19 -0500

    Agregar detalle del programador

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args){
 	System.out.println("¡Hola Mundo!");
 	System.out.println("Bienvenidos a Pweb2 :D");
+	System.out.println("Atte: Eduardo Franshua Durand Obando");
     }
 }
